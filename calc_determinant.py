f3bf77944ef97d24bf1e63de8d6605067a83c87f
ZapAnton <wc3ft2@gmail.com>
2018-02-09 17:56:47 +0300

    Added matrix initialization through user input

--- calc_determinant.py
+++ calc_determinant.py
@@ -1,2 +1,21 @@
 if __name__ == '__main__':
-    print('Hello!')
+    try:
+        matrix_size = int(input('Enter matrix size: '))
+
+        matrix = []
+
+        print('Enter matrix elements:\n')
+
+        for i in range(matrix_size):
+            matrix_row = []
+
+            for j in range(matrix_size):
+                matrix_element = int(input())
+
+                matrix_row.append(matrix_element)
+
+            matrix.append(matrix_row)
+
+        print(matrix)
+    except ValueError:
+        print('You should enter integer value for matrix size!')
