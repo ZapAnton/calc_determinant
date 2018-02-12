4bc3567b7baa18a08272d66662f5020431876978
ZapAnton <wc3ft2@gmail.com>
2018-02-12 12:25:46 +0300

    Implemented python determinant calculation for 1, 2, 3 dimension matrix

--- calc_determinant_PY.py
+++ calc_determinant_PY.py
@@ -1,2 +1,16 @@
 def calc_determinant(matrix):
+    matrix_size = len(matrix)
+
+    if matrix_size == 1:
+        return matrix[0]
+    elif matrix_size == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    elif matrix_size == 3:
+        return   matrix[0][0] * matrix[1][1] * matrix[2][2] \
+               + matrix[0][1] * matrix[1][2] * matrix[2][0] \
+               + matrix[0][2] * matrix[1][0] * matrix[2][1] \
+               - matrix[0][0] * matrix[1][2] * matrix[2][1] \
+               - matrix[0][1] * matrix[1][0] * matrix[2][2] \
+               - matrix[0][2] * matrix[1][1] * matrix[2][0]
+
     return 0
